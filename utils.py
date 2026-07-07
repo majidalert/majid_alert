2356413db2ac4f754368e51078704830c36b8530
majidalert <saeedbahal34@gmail.com>
2026-07-07 23:18:20 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,6 @@
 from datetime import datetime
 
 
-def percent_change(old_price: float, new_price: float) -> float:
-    if old_price == 0:
-        return 0
-    return ((new_price - old_price) / old_price) * 100
-
-
 def format_number(value):
     try:
         value = float(value)
@@ -20,7 +14,7 @@ def format_number(value):
         if value >= 1_000:
             return f"{value/1_000:.2f}K"
 
-        return f"{value:.2f}"
+        return f"{value:.4f}"
 
     except:
         return str(value)
@@ -33,11 +27,12 @@ def now():
 def make_message(title, symbol, price, change, volume, score):
 
     return (
+        f"🚨 MAJID ALERT AI\n\n"
         f"{title}\n\n"
-        f"🪙 {symbol}\n"
-        f"💲 Price : {price}\n"
-        f"📊 Change : {change:.2f}%\n"
-        f"📦 Volume : {format_number(volume)}\n"
-        f"⭐ Score : {score}/100\n"
+        f"🪙 ارز: {symbol}\n"
+        f"💰 قیمت: {price}\n"
+        f"📈 تغییر: {change:.2f}%\n"
+        f"📦 حجم: {format_number(volume)}\n"
+        f"⭐ امتیاز: {score}/100\n"
         f"🕒 {now()}"
     )
