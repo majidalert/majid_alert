1dec7101d51f028d2d9519c9a5742e50f4d4b758
majidalert <saeedbahal34@gmail.com>
2026-07-09 21:48:48 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 
+
 # ===========================
 # Telegram
 # ===========================
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+
 
 # ===========================
 # Scanner
@@ -15,11 +17,13 @@ SCAN_INTERVAL = int(
     os.getenv("SCAN_INTERVAL", "60")
 )
 
+
 # ===========================
 # Bybit
 # ===========================
 
 BASE_URL = "https://api.bybit.com"
+
 
 # ===========================
 # Alert Settings
@@ -29,30 +33,51 @@ PUMP_PERCENT = 10.0
 
 DUMP_PERCENT = -10.0
 
+
 # حداقل رشد از کف
+
 MIN_RISE_FROM_LOW = 50.0
 
+
 # حداقل امتیاز برای هشدار
+
 MIN_SCORE = 60
+
+
 
 # ===========================
 # مقاومت ها
 # ===========================
 
-# فاصله تا مقاومت روزانه
+# فاصله نزدیک شدن به مقاومت 4 ساعته
+
+FOUR_HOUR_RESISTANCE_DISTANCE = 2.0
+
+
+# فاصله نزدیک شدن به مقاومت روزانه
+
 DAILY_RESISTANCE_DISTANCE = 2.0
 
-# فاصله تا مقاومت ۳ روزه
+
+# فاصله نزدیک شدن به مقاومت هفتگی
+
+WEEKLY_RESISTANCE_DISTANCE = 5.0
+
+
+
+# فاصله نزدیک شدن به مقاومت 3 روزه
+
 THREE_DAY_RESISTANCE_DISTANCE = 3.0
 
-# فاصله تا مقاومت هفتگی
-WEEKLY_RESISTANCE_DISTANCE = 5.0
+
 
 # ===========================
 # حجم
 # ===========================
 
 VOLUME_MULTIPLIER = 2.0
+
+
 
 # ===========================
 # جلوگیری از هشدار تکراری
